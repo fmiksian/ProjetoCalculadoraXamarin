3512f763abffd9a3c11fc5e0ae240f2362472931
fmiksian@gmail.com <fmiksian@gmail.com>
2020-02-25 21:47:21 -0300

    atualização do projeto

---
+++ CalculadoraXamarin/CalculadoraXamarin/Calculadora.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CalculadoraXamarin
+{
+  public  class Calculadora
+    {
+        private bool blnDecimal, blnOperador;
+        private double dblValorVisor, dblOperando1, dblOperando2, dblResultado;
+        private int intDigitos, intFavorDivisao;
+        private char chrOperador;
+        private string strValorVisor;
+
+        public Calculadora()
+        {
+            blnDecimal = false;
+            blnOperador = false;
+            intFavorDivisao = 1;
+            dblValorVisor = 0;
+            dblOperando1 = 0;
+            dblOperando2 = 0;
+            dblResultado = 0;
+            intDigitos = 0;
+            chrOperador = ' ';
+            strValorVisor = "0";
+
+
+        }
+
+        public string ValorVisor
+        {
+            get { return strValorVisor; }
+
+        }
+        private void ConverterValor()
+        {
+
+            strValorVisor = String.Format(new CultureInfo("pr-BR"), "{0:0:00}", dblValorVisor);
+        }
+
+        private void Inicializar()
+        {
+            dblOperando1 = 0.0;
+            dblOperando2 = 0.0;
+            intDigitos = 0;
+            intFavorDivisao = 1;
+            blnDecimal = false;
+            blnOperador = false;
+            chrOperador = ' ';
+
+        }
+        private int Fatorial(int intValor)
+        {
+            int intResultado = 1;
+            for (int intContador = 2; intContador <= intValor; intContador++)
+                intResultado *= intContador;
+            return intResultado;
+
+        }
+
+        private double ConverterRadianos(double dblValorAngulo) => (dblValorAngulo * 0.0174532925);
+        public void TeclaNumeros(double dblDigitos)
+        {
+            if (intDigitos <= 10)
+            {
+                if (blnDecimal)
+                {
+                    intFavorDivisao *= 10;
+                    dblValorVisor += (dblDigitos / intFavorDivisao);
+
+                }
+                else
+                    dblValorVisor = (dblValorVisor * 10) + dblDigitos;
+                intDigitos++;
+                ConverterValor();
+            }
+        }
+
+        public void TeclaVirgula()
+        {
+            if (!blnDecimal)
+            {
+                blnDecimal = true;
+                intFavorDivisao = 1;
+                dblValorVisor /= intFavorDivisao;
+                ConverterValor();
+            }
+        }
+
+        public void TeclaIgual()
+        {
+            if (!blnOperador)
+            {
+                dblOperando1 = dblValorVisor;
+                dblValorVisor = 0.0;
+            }
+
+            else
+            {
+                dblOperando2 = dblValorVisor;
+                switch (chrOperador)
+                {
+                    case '/':
+                        if (dblOperando2 > 0.0)
+                            dblResultado = dblOperando1 / dblOperando2;
+                        break;
+                    case 'X':
+                        dblResultado = dblOperando1 - dblOperando2;
+                        break;
+                    case '-':
+                        dblResultado = dblOperando1 - dblOperando2;
+                        break;
+                    case '+':
+                        dblResultado = dblOperando1 + dblOperando2;
+                        break;
+                }
+                dblOperando1 = dblResultado;
+                dblValorVisor = dblResultado;
+            }
+            ConverterValor();
+            blnDecimal = false;
+        }
+
+        public void TeclaDivisao()
+        {
+            chrOperador = '/';
+            TeclaIgual();
+            blnOperador = true;
+        }
+
+        public void TeclaMultiplicacao()
+        {
+            chrOperador = 'X';
+            TeclaIgual();
+            blnOperador = true;
+        }
+
+        public void TeclaSubtracao()
+        {
+            chrOperador = '-';
+            TeclaIgual();
+            blnOperador = true;
+        }
+
+        public void TeclaAdicao()
+        {
+            chrOperador = '+';
+            TeclaIgual();
+            blnOperador = true;
+        }
+
+
+        public void TeclaLimparCE()
+        {
+            dblValorVisor = 0.0;
+            intDigitos = 0;
+            blnDecimal = false;
+            blnOperador = false;
+            ConverterValor();
+        }
+
+        public void TeclaLimparC()
+        {
+            dblValorVisor = 0.0;
+            dblResultado = 0.0;
+
+            Inicializar();
+            ConverterValor();
+        }
+
+        public void TeclaElevarQuadrado()
+        {
+
+            dblResultado = dblValorVisor * dblValorVisor;
+            dblValorVisor = dblResultado;
+            ConverterValor();
+            Inicializar();
+        }
+
+        public void TeclaRaizQuadrada()
+        {
+
+            dblResultado = Math.Sqrt(dblValorVisor);
+            dblValorVisor = dblResultado;
+            ConverterValor();
+            Inicializar();
+        }
+
+        public void TeclaSeno()
+        {
+
+            dblResultado = Math.Sin(ConverterRadianos(dblValorVisor));
+            dblValorVisor = dblResultado;
+            ConverterValor();
+            Inicializar();
+        }
+
+        public void TeclaCosseno()
+        {
+
+            dblResultado = Math.Cos(ConverterRadianos(dblValorVisor));
+            dblValorVisor = dblResultado;
+            ConverterValor();
+            Inicializar();
+        }
+
+        public void TeclaTangente()
+        {
+
+            dblResultado = Math.Tan(ConverterRadianos(dblValorVisor));
+            dblValorVisor = dblResultado;
+            ConverterValor();
+            Inicializar();
+        }
+
+        public void TeclaFatorial()
+        {
+
+            dblResultado = Fatorial((int)dblValorVisor);
+            dblValorVisor = dblResultado;
+            ConverterValor();
+            Inicializar();
+        }
+
+        public void TeclaConversao(int intOpcaoConversao)
+        {
+            double[] dblFatorMultiplicacao = { 2.54, 0.3937, 1.609, 0.6214, 16.387064, 0.0610237, 3.7854117, 0.2642, 0.4536, 2.205, 0.4047, 2.417 };
+            dblResultado = dblValorVisor * dblFatorMultiplicacao[intOpcaoConversao];
+            dblValorVisor = dblResultado;
+            ConverterValor();
+            Inicializar();
+        }
+    }
+}
--- CalculadoraXamarin/CalculadoraXamarin/MainActivity.cs
+++ CalculadoraXamarin/CalculadoraXamarin/MainActivity.cs
@@ -6,14 +6,171 @@ using Android.Widget;
 
 namespace CalculadoraXamarin
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "MultiCalcV1", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        TextView txtVisor;
+        Button btnNumero0, btnNumero1, btnNumero2, btnNumero3, btnNumero4,
+            btnNumero5, btnNumero6, btnNumero7, btnNumero8,
+            btnNumero9, btnVirgula, btnIgual, btnSomar,
+            btnSubtrair, btnMultiplicar, btnDividir, btnLimparCE,
+            btnLimparC, btnElevarQuadrado, btnRaizQuadrada, btnSeno,
+            btnCosseno, btnTangente, btnFatorial, btnConverter;
+        Spinner spnConversao;
+        Calculadora calculadora;
+        int intOpcaoConversao = 0;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            txtVisor = FindViewById<TextView>(Resource.Id.txtVisor);
+            //Teclas Numericas
+            btnNumero0 = FindViewById<Button>(Resource.Id.btnNumero0);
+            btnNumero1 = FindViewById<Button>(Resource.Id.btnNumero1);
+            btnNumero2 = FindViewById<Button>(Resource.Id.btnNumero2);
+            btnNumero3 = FindViewById<Button>(Resource.Id.btnNumero3);
+            btnNumero4 = FindViewById<Button>(Resource.Id.btnNumero4);
+            btnNumero5 = FindViewById<Button>(Resource.Id.btnNumero5);
+            btnNumero6 = FindViewById<Button>(Resource.Id.btnNumero6);
+            btnNumero7 = FindViewById<Button>(Resource.Id.btnNumero7);
+            btnNumero8 = FindViewById<Button>(Resource.Id.btnNumero8);
+            btnNumero9 = FindViewById<Button>(Resource.Id.btnNumero9);
+            //Teclas de virgula e sinal de igual
+            btnVirgula = FindViewById<Button>(Resource.Id.btnVirgula);
+            btnIgual = FindViewById<Button>(Resource.Id.btnIgual);
+            //Tecla de Operadores
+            btnSomar = FindViewById<Button>(Resource.Id.btnSomar);
+            btnSubtrair = FindViewById<Button>(Resource.Id.btnSubtrair);
+            btnMultiplicar = FindViewById<Button>(Resource.Id.btnMultiplicar);
+            btnDividir = FindViewById<Button>(Resource.Id.btnDivdir);
+            //Teclas de Limpeza
+            btnLimparCE = FindViewById<Button>(Resource.Id.btnLimparCE);
+            btnLimparC = FindViewById<Button>(Resource.Id.btnLimparC);
+            //Teclas de funções matemáticas
+            btnElevarQuadrado = FindViewById<Button>(Resource.Id.btnElevarQuadrado);
+            btnRaizQuadrada = FindViewById<Button>(Resource.Id.btnRaizQuadrada);
+            btnSeno = FindViewById<Button>(Resource.Id.btnSeno);
+            btnCosseno = FindViewById<Button>(Resource.Id.btnCosseno);
+            btnTangente = FindViewById<Button>(Resource.Id.btnTangente);
+            btnFatorial = FindViewById<Button>(Resource.Id.btnFatorial);
+            //conversão de unidades de medida
+            spnConversao = FindViewById<Spinner>(Resource.Id.spnConversao);
+            btnConverter = FindViewById<Button>(Resource.Id.btnConverter);
+
+            calculadora = new Calculadora();
+
+            btnNumero0.Click += OnTeclaNumeros;
+            btnNumero1.Click += OnTeclaNumeros;
+            btnNumero2.Click += OnTeclaNumeros;
+            btnNumero3.Click += OnTeclaNumeros;
+            btnNumero4.Click += OnTeclaNumeros;
+            btnNumero5.Click += OnTeclaNumeros;
+            btnNumero6.Click += OnTeclaNumeros;
+            btnNumero7.Click += OnTeclaNumeros;
+            btnNumero8.Click += OnTeclaNumeros;
+            btnNumero9.Click += OnTeclaNumeros;
+            btnVirgula.Click += OnTeclaVirgula;
+            btnIgual.Click += OnTeclaIgual;
+            btnSomar.Click += OnTeclaSomar;
+            btnSubtrair.Click += OnTeclaSubtrair;
+            btnMultiplicar.Click += OnTeclaMultiplicar;
+            btnDividir.Click += OnTeclaDividir;
+            btnLimparCE.Click += OnTeclaLimparCE;
+            btnLimparC.Click += OnTeclaLimparC;
+            btnElevarQuadrado.Click += OnTeclaElevarQuadrado;
+            btnRaizQuadrada.Click += OnTeclaRaizQuadrada;
+            btnSeno.Click += OnTeclaSeno;
+            btnCosseno.Click += OnTeclaCosseno;
+            btnTangente.Click += OnTeclaTangente;
+            btnFatorial.Click += OnTeclaFatorial;
+            btnConverter.Click += OnteclaConversao;
+            spnConversao.ItemSelected += new System.EventHandler<AdapterView.ItemSelectedEventArgs>(OnItemSelecionado);
+
+        }
+
+        private void OnItemSelecionado(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            intOpcaoConversao = e.Position;
+        }
+        private void OnteclaConversao(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnTeclaFatorial(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnTeclaTangente(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnTeclaCosseno(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnTeclaSeno(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnTeclaRaizQuadrada(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnTeclaElevarQuadrado(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnTeclaLimparC(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnTeclaLimparCE(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnTeclaDividir(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnTeclaMultiplicar(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnTeclaSubtrair(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnTeclaSomar(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnTeclaIgual(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnTeclaVirgula(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnTeclaNumeros(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
